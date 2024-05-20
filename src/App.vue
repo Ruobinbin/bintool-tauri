575a36a00bd0b0cf96999389097b3a9db98f7dd1
Ruobin <1773274301@qq.com>
2024-05-21 05:23:30 +0800

    透明和input焦点

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watchEffect } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 import { Window } from "@tauri-apps/api/window";
 import { register } from '@tauri-apps/plugin-global-shortcut';
 
-const input_value = ref("");
+const currentWindow = Window.getCurrent();
+let inputElement = ref<HTMLInputElement>();
+const input = ref("");
 async function onEnter() {
-    console.log(await invoke("input_enter", { value: input_value.value }))
+    console.log(await invoke("input_enter", { value: input.value }))
 }
+
+// 当 input 元素失去焦点时，隐藏窗口
+watchEffect(() => {
+    inputElement.value?.addEventListener("blur", () => {
+        currentWindow.hide();
+    });
+});
 
 onMounted(async () => {
     //快捷键控制窗口
-    const currentWindow = Window.getCurrent();
     await register('CommandOrControl+Shift+A', async (event) => {
         if (event.state === 'Pressed') {
             if (await currentWindow.isVisible()) {
                 currentWindow.hide();
             } else {
-                currentWindow.show();
+                await currentWindow.show();
+                await currentWindow.setFocus();
+                inputElement.value?.focus();
             }
         }
     });
 })
-
 </script>
 
 <template>
-    <input :style="{ width: '80%', margin: '0 auto', display: 'block' }" @keyup.enter="onEnter" v-model="input_value" />
+    <input :style="{ height: '40px', width: '80%', margin: '0 auto', display: 'block' }" @keyup.enter="onEnter"
+        v-model="input" ref="inputElement" />
 </template>
 
 <style scoped></style>
