cbf3e1df1cc19265ca4637639b181f744fe89924
Ruobin <1773274301@qq.com>
2024-05-22 11:52:06 +0800

    启动时不显示input

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, watchEffect } from "vue";
+import { onMounted, ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 import { Window } from "@tauri-apps/api/window";
 import { register } from '@tauri-apps/plugin-global-shortcut';
@@ -11,19 +11,16 @@ async function onEnter() {
     console.log(await invoke("input_enter", { value: input.value }))
 }
 
-// 当 input 元素失去焦点时，隐藏窗口
-watchEffect(() => {
-    inputElement.value?.addEventListener("blur", () => {
-        currentWindow.hide();
-    });
-});
-
 onMounted(async () => {
+    // 当 input 元素失去焦点时，隐藏窗口
+    inputElement.value?.addEventListener("blur", async () => {
+        await currentWindow.hide();
+    });
     //快捷键控制窗口
     await register('CommandOrControl+Shift+A', async (event) => {
         if (event.state === 'Pressed') {
             if (await currentWindow.isVisible()) {
-                currentWindow.hide();
+                await currentWindow.hide();
             } else {
                 await currentWindow.show();
                 await currentWindow.setFocus();
