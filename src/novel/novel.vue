c42e32cdfac854b0b2dd1a94cea69d1d3695e1d0
Ruobin <1773274301@qq.com>
2024-05-30 11:58:14 +0800

    novel窗口创建

---
+++ src/novel/novel.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-tabs tab-position="left">
+        <el-tab-pane label="User">User</el-tab-pane>
+        <el-tab-pane label="Config">Config</el-tab-pane>
+        <el-tab-pane label="Role">Role</el-tab-pane>
+        <el-tab-pane label="Task">Task</el-tab-pane>
+    </el-tabs>
+</template>
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
