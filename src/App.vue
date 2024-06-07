ed3756c19421ca16ec8cc24792edaf88d41a4da3
Ruobin <1773274301@qq.com>
2024-06-07 19:59:21 +0800

    测试更新0.0.2

--- src/App.vue
+++ src/App.vue
@@ -9,55 +9,54 @@ import { relaunch } from '@tauri-apps/plugin-process';
 
 const currentWindow = Window.getCurrent();
 let inputElement = ref<HTMLInputElement>();
-const input = ref("");
+const input = ref("0.0.2");
 async function onEnter() {
-    switch (input.value) {
-        case '/xs':
-            if (!WebviewWindow.getByLabel('novel')) {
-                new WebviewWindow('novel', {
-                    url: 'src/novel/novel.html'
-                })
-            } else {
-                WebviewWindow.getByLabel('novel')?.unminimize()
-                WebviewWindow.getByLabel('novel')?.setFocus()
-            }
-            break;
-        default:
-            console.log(await invoke("input_enter", { value: input.value }))
-    }
+  switch (input.value) {
+    case '/xs':
+      if (!WebviewWindow.getByLabel('novel')) {
+        new WebviewWindow('novel', {
+          url: 'src/novel/novel.html'
+        })
+      } else {
+        WebviewWindow.getByLabel('novel')?.unminimize()
+        WebviewWindow.getByLabel('novel')?.setFocus()
+      }
+      break;
+    default:
+      console.log(await invoke("input_enter", { value: input.value }))
+  }
 }
 
 onMounted(async () => {
-    // 当 input 元素失去焦点时，隐藏窗口
-    inputElement.value?.addEventListener("blur", async () => {
+  // 当 input 元素失去焦点时，隐藏窗口
+  inputElement.value?.addEventListener("blur", async () => {
+    await currentWindow.hide();
+  });
+  //快捷键控制窗口
+  await register('CommandOrControl+Shift+A', async (event) => {
+    if (event.state === 'Pressed') {
+      if (await currentWindow.isVisible()) {
         await currentWindow.hide();
-    });
-    //快捷键控制窗口
-    await register('CommandOrControl+Shift+A', async (event) => {
-        if (event.state === 'Pressed') {
-            if (await currentWindow.isVisible()) {
-                await currentWindow.hide();
-            } else {
-                await currentWindow.show();
-                await currentWindow.setFocus();
-                inputElement.value?.focus();
-            }
-        }
-    });
-    const update = await check();
-    console.log(update)
-    if (update?.available) {
-        console.log(`Update to ${update.version} available! Date: ${update.date}`);
-        console.log(`Release notes: ${update.body}`);
-        await update.downloadAndInstall();
-        await relaunch();
+      } else {
+        await currentWindow.show();
+        await currentWindow.setFocus();
+        inputElement.value?.focus();
+      }
     }
+  });
+  const update = await check();
+  if (update?.available) {
+    console.log(`Update to ${update.version} available! Date: ${update.date}`);
+    console.log(`Release notes: ${update.body}`);
+    await update.downloadAndInstall();
+    await relaunch();
+  }
 })
 </script>
 
 <template>
-    <input :style="{ height: '40px', width: '80%', margin: '0 auto', display: 'block' }" @keyup.enter="onEnter"
-        v-model="input" ref="inputElement" />
+  <input :style="{ height: '40px', width: '80%', margin: '0 auto', display: 'block' }" @keyup.enter="onEnter"
+    v-model="input" ref="inputElement" />
 </template>
 
 <style scoped></style>
