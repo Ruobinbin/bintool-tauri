abdcb1e67e51f94d58664fbe600c0373880e3c10
Ruobin <1773274301@qq.com>
2024-07-29 08:55:08 +0800

    初始化目录和开启sovits等方法

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,75 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+mod utils;
+
+use once_cell::sync::OnceCell;
+use std::path::PathBuf;
 use tauri::{
     menu::{MenuBuilder, MenuItemBuilder},
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
     Manager,
 };
 
+static APP_RESOURCE_DIR: OnceCell<PathBuf> = OnceCell::new(); //app所在目录
+static USER_FILES_DIR: OnceCell<PathBuf> = OnceCell::new(); //用户文件所在目录
+pub static GPT_SOVITS_MODEL_DIR: OnceCell<PathBuf> = OnceCell::new(); //gpt-sovits模型所在目录
+pub static NOVEL_OUTPUT_DIR: OnceCell<PathBuf> = OnceCell::new(); //小说输出目录
+
 #[tauri::command]
 fn input_enter(value: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", value)
+}
+
+//判断容器是否运行
+#[tauri::command]
+async fn is_container_running(container_name: &str) -> Result<bool, String> {
+    utils::bollard_utils::is_container_running(container_name)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+//开启gpt-sovits容器
+#[tauri::command]
+async fn start_gpt_sovits() -> Result<String, String> {
+    utils::bollard_utils::start_gpt_sovits()
+        .await
+        .map_err(|e| e.to_string())
+}
+
+//保存小说音频
+#[tauri::command]
+async fn save_novel_audio(audio_data: Vec<u8>) -> Result<String, String> {
+    let timestamp = utils::default_utils::current_timestamp();
+    let file_name = format!("novel_audio_{}.wav", timestamp);
+    let file_path = NOVEL_OUTPUT_DIR.get().unwrap().join(file_name);
+    match utils::default_utils::write_audio_to_file(audio_data, file_path.clone()) {
+        Ok(_) => Ok(file_path.to_string_lossy().to_string()),
+        Err(e) => Err(e.to_string()),
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            let resources_dir = app.path().resource_dir().unwrap();
+            // 初始化数据目录
+            APP_RESOURCE_DIR
+                .set(utils::default_utils::remove_long_path_prefix(
+                    &resources_dir,
+                ))
+                .unwrap(); //app所在目录
+            USER_FILES_DIR
+                .set(APP_RESOURCE_DIR.get().unwrap().join("user_files"))
+                .unwrap(); //用户文件所在目录
+            GPT_SOVITS_MODEL_DIR
+                .set(USER_FILES_DIR.get().unwrap().join("gpt_sovits_model"))
+                .unwrap(); //gpt-sovits模型所在目录
+            NOVEL_OUTPUT_DIR
+                .set(USER_FILES_DIR.get().unwrap().join("novel_output"))
+                .unwrap();
+            utils::default_utils::ensure_path_exists(USER_FILES_DIR.get().unwrap()).unwrap();
+            utils::default_utils::ensure_path_exists(GPT_SOVITS_MODEL_DIR.get().unwrap()).unwrap();
+            utils::default_utils::ensure_path_exists(NOVEL_OUTPUT_DIR.get().unwrap()).unwrap();
             //系统托盘
             let quit = MenuItemBuilder::with_id("quit", "退出").build(app)?;
             let menu = MenuBuilder::new(app).items(&[&quit]).build()?;
@@ -45,7 +101,12 @@ pub fn run() {
         .plugin(tauri_plugin_sql::Builder::default().build()) // SQLite数据库
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init()) // HTTP请求
-        .invoke_handler(tauri::generate_handler![input_enter])
+        .invoke_handler(tauri::generate_handler![
+            input_enter,
+            is_container_running,
+            start_gpt_sovits,
+            save_novel_audio
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
