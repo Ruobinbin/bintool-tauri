45abf3a2be7130bf4a2af507720bae18150ebac6
Ruobin <1773274301@qq.com>
2024-07-29 08:57:51 +0800

    生成音频完成

--- src/novel/novel.vue
+++ src/novel/novel.vue
@@ -12,22 +12,139 @@
             <br />
             <el-input v-model="fqNovelContent" style="width: 100%" autosize type="textarea" placeholder="小说内容" />
         </el-tab-pane>
-        <el-tab-pane label="Config">Config</el-tab-pane>
-        <el-tab-pane label="Role">Role</el-tab-pane>
-        <el-tab-pane label="Task">Task</el-tab-pane>
+        <el-tab-pane label="生成音频">
+            <el-button :loading="IsGptSovitsLoading" @click="startGptSovits" :disabled=IsGptSovitsRunning>
+                {{ IsGptSovitsRunning ? "sovit已开启" : "开启sovits" }}
+            </el-button>
+            <el-button @click="setNovelTable">清空并且设置表格</el-button>
+            <el-button @click="insertNovel(-1)">插入</el-button>
+            <br />
+            <el-table :data="novels">
+                <el-table-column label="索引" :min-width="20">
+                    <template #default="scope">
+                        {{ scope.$index }}
+                    </template>
+                </el-table-column>
+                <el-table-column prop="content" label="content">
+                    <template #default="scope">
+                        <el-input v-model="scope.row.content" placeholder="请输入内容"></el-input>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="audioSrc" label="audioSrc">
+                    <template #default="scope">
+                        <audio :src="convertFileSrc(scope.row.audioSrc)" controls></audio>
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template #default="scope">
+                        <el-button @click="generateAudio(scope.$index)" :loading="scope.row.loading">生成音频</el-button>
+                        <el-button @click="delNovel(scope.$index)">删除</el-button>
+                        <el-button @click="insertNovel(scope.$index)">插入</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="Role">
+        </el-tab-pane>
+        <el-tab-pane label="Task">
+        </el-tab-pane>
     </el-tabs>
 </template>
 <script lang="ts" setup>
 import { fetch } from '@tauri-apps/plugin-http';
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { stripHtmlTags } from '../utils/defaultUtils'
-import { formatNovelText } from '../utils/novelUtils'
+import { formatNovelText, Novel, fetchNovels } from '../utils/novelUtils'
+import { invoke, convertFileSrc } from '@tauri-apps/api/core';
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { getCurrent } from "@tauri-apps/api/window";
+import Database from '@tauri-apps/plugin-sql';
 
 const fqNovelIsLoading = ref(false) //番茄小说是否在获取
 const fqNovelId = ref('') //番茄小说id
 const fqNovelApi = ref('https://fqnovel.pages.dev/content?item_id=') //番茄小说api地址
 const fqNovelContent = ref('') //番茄小说内容
 const fqNovelCount = computed(() => { return fqNovelContent.value.length }) //番茄小说字数
+const novels = ref<Novel[]>([])
+const IsGptSovitsRunning = ref(false) //是否开启sovits
+const IsGptSovitsLoading = ref(false) //sovits是否正在开启
+
+//载入时触发
+onMounted(async () => {
+    //创建数据库
+    const db = await Database.load('sqlite:bintool.db');
+    await db.execute(`
+      CREATE TABLE IF NOT EXISTS novels (
+        id INTEGER PRIMARY KEY,
+        audioSrc TEXT NOT NULL,
+        content TEXT NOT NULL
+      )
+    `);
+    //初始化novels
+    novels.value = await fetchNovels()
+    //初始化sovits状态
+    await invoke('is_container_running', { containerName: 'gpt-sovits' })
+        .then((is_running) => {
+            IsGptSovitsRunning.value = is_running as boolean;
+        })
+        .catch((error) => {
+            IsGptSovitsRunning.value = false;
+            ElMessage({
+                message: error as string,
+                type: 'error',
+            });
+        });
+    //监听窗口关闭事件
+    await getCurrent().onCloseRequested(async () => {
+        await db.execute('DELETE FROM novels');
+        for (const novel of novels.value) {
+            await db.execute('INSERT INTO novels (content , audioSrc) VALUES (?, ?)', [novel.content, novel.audioSrc]);
+        }
+    });
+});
+
+// 开启sovits
+const startGptSovits = async () => {
+    IsGptSovitsLoading.value = true
+    await invoke("start_gpt_sovits").then((message) => {
+        ElMessage({
+            message: message as string,
+            type: 'success',
+        })
+        IsGptSovitsRunning.value = true
+    }).catch((error) => {
+        ElMessage({
+            message: error as string,
+            type: 'error',
+        })
+    })
+    IsGptSovitsLoading.value = false
+}
+// 生成音频
+const generateAudio = async (novelIndex: number) => {
+    if (!IsGptSovitsRunning.value) {
+        ElMessage({
+            message: 'GPT-Sovits未开启',
+            type: 'error',
+        })
+        return
+    }
+    await novels.value[novelIndex].generateAudio()
+        .catch((error) => {
+            ElMessage({
+                message: error as string,
+                type: 'error',
+            });
+        });
+}
+// 删除小说
+const delNovel = (novelIndex: number) => {
+    novels.value.splice(novelIndex, 1)
+}
+// 插入小说
+const insertNovel = (novelIndex: number) => {
+    novels.value.splice(novelIndex + 1, 0, new Novel(''));
+}
 
 // 获取番茄小说
 const getFqNovel = async () => {
@@ -46,6 +163,37 @@ const getFqNovel = async () => {
     fqNovelContent.value = formatNovelText(stripHtmlTags(text))
     fqNovelIsLoading.value = false
 }
+
+// 设置表格
+const setNovelTable = () => {
+    ElMessageBox.confirm(
+        '此操作会清空表格, 是否继续?',
+        'Warning',
+        {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning',
+        }
+    )
+        .then(() => {
+            if (fqNovelContent.value) {
+                novels.value = []
+                fqNovelContent.value.split('\n').map((content) => {
+                    let novel = new Novel(content)
+                    novels.value.push(novel)
+                })
+            }
+        })
+        .catch(() => {
+            ElMessage({
+                type: 'info',
+                message: '已取消',
+            })
+        })
+}
+
+
+
 </script>
 
 <style></style>
--- src/utils/novelUtils.ts
+++ src/utils/novelUtils.ts
@@ -1,3 +1,6 @@
+import Database from '@tauri-apps/plugin-sql';
+import { fetch } from '@tauri-apps/plugin-http';
+import { invoke } from '@tauri-apps/api/core';
 //格式话小说文本
 function formatNovelText(text: string): string {
     const regex = new RegExp(`([。])([”】]?)`, 'g');
@@ -7,4 +10,59 @@ function formatNovelText(text: string): string {
     return newText;
 }
 
-export { formatNovelText };
+async function fetchNovels(): Promise<Novel[]> {
+    try {
+        const db = await Database.load('sqlite:bintool.db');
+        const result = await db.select('SELECT * FROM novels') as INovel[]
+        const novels = result.map(data => new Novel(data.content, data.audioSrc));
+        return novels;
+    } catch (error) {
+        throw error;
+    }
+}
+
+interface INovel {
+    content: string;
+    audioSrc: string;
+    loading: boolean;
+    printDetails(): void;
+    generateAudio(): Promise<void>;
+}
+
+class Novel implements INovel {
+    loading: boolean;
+
+    constructor(public content: string, public audioSrc: string = '未生成') {
+        this.loading = false;
+    }
+
+    printDetails(): void {
+        console.log(`Content: ${this.content}, Audio Source: ${this.audioSrc}`);
+    }
+
+    async generateAudio(): Promise<void> {
+        this.loading = true;
+        try {
+            const response = await fetch("http://127.0.0.1:9880/", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    text: this.content,
+                    text_language: 'zh'
+                })
+            });
+            const audioBlob = await response.blob();
+            const audioData = Array.from(new Uint8Array(await audioBlob.arrayBuffer()));
+            this.audioSrc = await invoke("save_novel_audio", { audioData }) as string;
+        } catch (error) {
+            throw error;
+        } finally {
+            this.loading = false;
+        }
+    }
+}
+
+export { Novel, formatNovelText, fetchNovels };
+export type { INovel };
