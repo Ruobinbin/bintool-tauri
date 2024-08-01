3b339bb0ceee4adcf501d4acb0a6548957f65852
Ruobin <1773274301@qq.com>
2024-08-01 11:07:29 +0800

    添加打开路径方法

--- src-tauri/src/utils/default_utils.rs
+++ src-tauri/src/utils/default_utils.rs
@@ -1,7 +1,7 @@
+use std::fs::{self, File};
 use std::io::Write;
 use std::path::PathBuf;
-use std::fs::{self, File};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::process::Command;
 
 //创建目录
 pub fn ensure_path_exists(path: &PathBuf) -> std::io::Result<()> {
@@ -28,8 +28,23 @@ pub fn write_audio_to_file(audio_data: Vec<u8>, file_path: PathBuf) -> Result<()
     Ok(())
 }
 
-// 获取当前时间戳
-pub fn current_timestamp() -> u64 {
-    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
-}
+//打开路径
+pub fn open_path(path: PathBuf) -> Result<(), String> {
+    if !path.exists() {
+        return Err(format!("路径不存在: {:?}", path));
+    }
 
+    let output = Command::new("explorer")
+        .arg(path.as_os_str())
+        .output()
+        .map_err(|e| format!("执行命令失败: {}", e))?;
+
+    if !output.status.success() {
+        return Err(format!(
+            "命令执行失败: {}",
+            String::from_utf8_lossy(&output.stderr)
+        ));
+    }
+
+    Ok(())
+}
