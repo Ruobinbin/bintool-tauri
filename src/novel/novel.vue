d218aebb18630c6f2995cdd408e4e5a90cf90c6f
Ruobin <1773274301@qq.com>
2024-08-25 10:00:04 +0800

    选择视频

--- src/novel/novel.vue
+++ src/novel/novel.vue
@@ -47,6 +47,33 @@
             <gpt_sovits_model />
         </el-tab-pane>
         <el-tab-pane label="视频">
+            <audio src=""></audio>
+            <el-input v-model="channelUrl" placeholder="博主主页链接"></el-input>
+            <el-button @click="get_video_list">获取视频列表</el-button>
+            <audio ref="audios" :src="convertFileSrc(OUTPUT_PATH + '\\audio.wav')" controls></audio>
+            <div style="margin-bottom: 100px;">
+                已选视频总时长{{ selected_video_list_duration }}
+                <div style="display: flex;flex-wrap: wrap;gap: 10px;">
+                    <div style="background: lightcoral;flex: 0 1 auto;width: 200px;border-radius: 10px;"
+                        v-for="video in selected_video_list" :key="video.id">
+                        <img v-if="video.thumbnails.length > 0" :src="video.getLargestThumbnailUrl()" :alt="video.url"
+                            @click="del_video(video)" style="width: 100%; height: auto; border-radius: 10px;" />
+                        <a :href="video.url" @click.prevent="open(video.url)">{{ video.url }}</a>
+                        <p>时长: {{ video.duration }} 秒</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div style="display: flex;flex-wrap: wrap;gap: 10px;">
+                    <div style="background: lightgrey;flex: 0 1 auto;width: 200px;border-radius: 10px;"
+                        v-for="video in video_list" :key="video.id">
+                        <img v-if="video.thumbnails.length > 0" :src="video.getLargestThumbnailUrl()" :alt="video.url"
+                            @click="sel_video(video)" style="width: 100%; height: auto; border-radius: 10px;" />
+                        <a :href="video.url" @click.prevent="open(video.url)">{{ video.url }}</a>
+                        <p>时长: {{ video.duration }} 秒</p>
+                    </div>
+                </div>
+            </div>
         </el-tab-pane>
         <el-tab-pane label="最后合成">
             <el-button @click="test">test</el-button>
@@ -55,6 +82,7 @@
 </template>
 <script lang="ts" setup>
 import { fetch } from '@tauri-apps/plugin-http';
+import { open } from '@tauri-apps/plugin-shell';
 import { invoke, convertFileSrc } from '@tauri-apps/api/core';
 import { listen } from '@tauri-apps/api/event';
 
@@ -66,18 +94,30 @@ import gpt_sovits_model from '../components/gpt_sovits_model.vue';
 import { computed, onMounted, ref } from 'vue'
 import { stripHtmlTags, getFileNameFromPath } from '../utils/defaultUtils'
 import { formatNovelText, Novel, fetchNovels } from '../utils/novelUtils'
+import { IThumbnail, Video } from '../utils/yt_dlp_uitls'
 import { ElMessage, ElMessageBox } from 'element-plus'
 
+const OUTPUT_PATH = ref('');
 const fqNovelIsLoading = ref(false) //番茄小说是否在获取
 const fqNovelId = ref('') //番茄小说id
 const fqNovelApi = ref('https://fqnovel.pages.dev/content?item_id=') //番茄小说api地址
 const fqNovelContent = ref('') //番茄小说内容
 const fqNovelCount = computed(() => { return fqNovelContent.value.length }) //番茄小说字数
-const novels = ref<Novel[]>([])
+const novels = ref<Novel[]>([])//小说
+const video_list = ref<Video[]>([]);//视频列表
+const selected_video_list = ref<Video[]>([]);//已选视频
+const selected_video_list_duration = computed(() => {
+    return selected_video_list.value.reduce((sum, video) => sum + video.duration, 0);
+});
 const isGptSovitsApiRunning = ref(false) //是否开启sovits
+const channelUrl = ref('https://www.youtube.com/@hetongxue') //视频主页链接
+const audios = ref<HTMLAudioElement>(); // 所有音频
+
+
 
 //载入时触发
 onMounted(async () => {
+    OUTPUT_PATH.value = await resourceDir() + '\\user_files\\novel_output';
     //创建数据库
     const db = await Database.load('sqlite:bintool.db');
     await db.execute(`
@@ -102,79 +142,90 @@ onMounted(async () => {
     });
 });
 
+const sel_video = (video: Video) => {
+    video_list.value = video_list.value.filter(v => v.id !== video.id);
+    selected_video_list.value.push(video);
+}
+
+const del_video = (video: Video) => {
+    selected_video_list.value = selected_video_list.value.filter(v => v.id !== video.id);
+    video_list.value.unshift(video);
+}
+
+//获取视频列表
+const get_video_list = async () => {
+    const cmd = [
+        '--flat-playlist',
+        '--print-json',
+        channelUrl.value
+    ];
+
+    const log = await invoke('run_yt_dlp_cmd', { cmd });
+    let logStr = log as string;
+    video_list.value = logStr.trim().split('\n').map((videoStr) => {
+        let video = JSON.parse(videoStr);
+        let thumbnails: IThumbnail[] = video.thumbnails
+        return new Video(video.id, video.url, video.duration, thumbnails);
+    });
+}
 const test = async () => {
-    let filePath = `${await resourceDir()}\\user_files\\novel_output\\audios.txt`;
-    let text = novels.value.map(novel => `file /workspace/novel_output/${getFileNameFromPath(novel.audioSrc)}`).join('\n');
-    await invoke('write_string_to_file', { text, filePath }).then(() => {
+    try {
+        // 生成用于合成全部音频的audio.txt文件
+        let filePath = `${await resourceDir()}\\user_files\\novel_output\\audios.txt`;
+        let text = novels.value.map(novel => `file /workspace/novel_output/${getFileNameFromPath(novel.audioSrc)}`).join('\n');
+        await invoke('write_string_to_file', { text, filePath });
         ElMessage({
             message: `生成文件 ${filePath} 成功`,
             type: 'success',
         });
-    }).catch((error) => {
-        ElMessage({
-            message: `生成文件 ${filePath} 失败: ${error as string}`,
-            type: 'error',
-        });
-        return;
-    });
 
-    let novelsTextFilePath = `${await resourceDir()}\\user_files\\novel_output\\text.txt`;
-    let novelsText = novels.value.map(novel => novel.content).join('\n');
-    await invoke('write_string_to_file', { text: novelsText, filePath: novelsTextFilePath }).then(() => {
+        // 音频合成
+        let audioSynthesiscmd: string[] = [
+            "-y",
+            "-f",
+            "concat",
+            "-safe",
+            "0",
+            "-i",
+            "/workspace/novel_output/audios.txt",
+            "-c:a",
+            "pcm_s16le",
+            "/workspace/novel_output/audios.wav"
+        ];
+        await invoke('run_ffmpeg_cmd', { cmd: audioSynthesiscmd });
+        audios.value!.src = convertFileSrc(OUTPUT_PATH.value + '\\audio.wav');
+        audios.value?.load();
+        ElMessage({
+            message: '已执行音频合成命令',
+            type: 'success',
+        });
+
+        // 生成字幕所需的txt文件
+        let novelsTextFilePath = `${await resourceDir()}\\user_files\\novel_output\\text.txt`;
+        let novelsText = novels.value.map(novel => novel.content).join('\n');
+        await invoke('write_string_to_file', { text: novelsText, filePath: novelsTextFilePath });
         ElMessage({
             message: `生成文件 ${novelsTextFilePath} 成功`,
             type: 'success',
         });
-    }).catch((error) => {
-        ElMessage({
-            message: `生成文件 ${novelsTextFilePath} 失败: ${error as string}`,
-            type: 'error',
-        });
-        return;
-    });
 
-    let audioSynthesiscmd: string[] = [
-        "-y",
-        "-f",
-        "concat",
-        "-safe",
-        "0",
-        "-i",
-        "/workspace/novel_output/audios.txt",
-        "-c:a",
-        "pcm_s16le",
-        "/workspace/novel_output/audio.wav"
-    ];
-
-    await invoke('run_ffmpeg_cmd', { cmd: audioSynthesiscmd }).then(() => {
-        ElMessage({
-            message: '已执行音频合成命令',
-            type: 'success',
-        })
-    }).catch((error) => {
-        ElMessage({
-            message: `合成audios.wav失败: ${error as string}`,
-            type: 'error',
-        })
-        return
-    })
-
-    let audioPath = "novel_output/audio.wav"
-    let textPath = "novel_output/text.txt"
-    let outputPath = "novel_output/audio_srt.srt"
-    await invoke('run_aeneas_cmd', { audioPath, textPath, outputPath }).then(() => {
+        // 字幕生成
+        let audioPath = "novel_output/audio.wav";
+        let textPath = "novel_output/text.txt";
+        let outputPath = "novel_output/audio_srt.srt";
+        await invoke('run_aeneas_cmd', { audioPath, textPath, outputPath });
         ElMessage({
             message: `生成文件 ${outputPath} 成功`,
             type: 'success',
-        })
-    }).catch((error) => {
+        });
+    } catch (error) {
         ElMessage({
-            message: `生成文件 ${outputPath} 失败: ${error as string}`,
+            message: `操作失败: ${error as string}`,
             type: 'error',
-        })
-        return
-    })
-}
+        });
+        return;
+    }
+};
 
 // 打开音频目录
 const openAudioDir = async () => {
@@ -206,7 +257,7 @@ const delNovel = (novelIndex: number) => {
 }
 // 插入小说
 const insertNovel = (novelIndex: number) => {
-    novels.value.splice(novelIndex + 1, 0, new Novel(''));
+    novels.value.splice(novelIndex + 1, 0, new Novel());
 }
 
 // 获取番茄小说
